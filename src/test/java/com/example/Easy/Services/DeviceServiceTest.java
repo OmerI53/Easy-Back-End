a45ee2e102aae565e2babb82a2c2edf765351ac6
Davut Büyükşimşek <buyuksimsek.davut@outlook.com>
2023-08-24 16:57:12 +0300

    deleteDevice

--- src/test/java/com/example/Easy/Services/DeviceServiceTest.java
+++ src/test/java/com/example/Easy/Services/DeviceServiceTest.java
@@ -1,16 +1,15 @@
 package com.example.Easy.Services;
 
 import com.example.Easy.Entities.DeviceEntity;
-import com.example.Easy.Models.DeviceDTO;
 import com.example.Easy.Models.DeviceType;
 import com.example.Easy.Repository.DeviceRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 import java.util.TimeZone;
 import java.util.UUID;
 
@@ -20,44 +19,11 @@ class DeviceServiceTest {
 
     @Autowired
     DeviceRepository deviceRepository;
-    @Autowired
-    DeviceService deviceService;
-
-    @Test
-    void bootstrap(){
-        DeviceEntity deviceEntity1 = DeviceEntity.builder()
-                .timeZone("GMT+03:00")
-                .deviceType(DeviceType.IOS)
-                .deviceToken("eYsHNOnxQ3K83vJB5vCkwQ:APA91bFp2N_MRa3penVKoSfFSCQl1er3FSZtfhTFKP-NcRWrtMIxhumE9Dp5NE2MoU2A-7Z-8XG_6LQ4r4GgPe3Nl7Q-byJBe92xbLZ3m_BGEMZb2xC_BkyDA63D_yzdqnd7RPxfJbZc")
-                .build();
-
-        DeviceEntity deviceEntity2 = DeviceEntity.builder()
-                .timeZone("GMT+04:00")
-                .deviceType(DeviceType.ANDROID)
-                .deviceToken("da37GPpPQR2t325Jg8sR-v:APA91bGcvhE_J3gX2udxEPJWgeR_M5e4y_jXpWlNozJnann2493qPn_1mewKchPvigMofwSLBH5PAr-msEL8DZbvURberbuA6667jsk-53JPvPlK5ugbvkSlP0CBADXW8xOLAbFq5lZa")
-                .build();
-
-        DeviceEntity deviceEntity3 = DeviceEntity.builder()
-                .timeZone("GMT+04:00")
-                .deviceType(DeviceType.MACOS)
-                .deviceToken("dGmSJfHQTxuXBNcMRz9Pv9:APA91bGtL-HlO088BAetJTkVc6TWQk2VpMNdNTspqvnwRziRJN-3oLIupE_ik6c8uO_b8kKP_56FibcLVdaKbcDQFV_UZ8x-tyVR9m2fMW_ZiCk3nblsX_TT7ZksxRSDPqmzg-UJRpje")
-                .build();
-
-        DeviceEntity deviceEntity4 = DeviceEntity.builder()
-                .timeZone("GMT+05:00")
-                .deviceType(DeviceType.WEB)
-                .deviceToken("fgiMSYssSZu_i4DjI3wxY5:APA91bH5Ylp92cYdZVBHoEqozkbBQ1-EGQI2JMTdmReLeMUFPqNsq1T8OIvBDasBGZjI_aFTeEhQU2lAtqGKVjong8YIUvBckiHvn-GjgVbvF9I0Ih1y316xtARCr_Ys50w8HE-Fy5nC")
-                .build();
-
-        deviceRepository.save(deviceEntity1);
-        deviceRepository.save(deviceEntity2);
-        deviceRepository.save(deviceEntity3);
-        deviceRepository.save(deviceEntity4);
-
-    }
 
     @Test
     void  addNewDeviceTest(){
+        String token = "eYsHNOnxQ3K83vJB5vCkwQ:APA91bFp2N_MRa3penVKoSfFSCQl1er3FSZtfhTFKP-NcRWrtMIxhumE9Dp5NE2MoU2A-7Z-8XG_6LQ4r4GgPe3Nl7Q-byJBe92xbLZ3m_BGEMZb2xC_BkyDA63D_yzdqnd7RPxfJbZc";
+        System.out.println(token.length());
         DeviceEntity deviceEntity = DeviceEntity.builder()
                 .timeZone("GMT+03:00")
                 .deviceType(DeviceType.ANDROID)
@@ -66,17 +32,31 @@ class DeviceServiceTest {
         deviceRepository.save(deviceEntity);
     }
     @Test
-    void getAlldevice(){
-        List<DeviceEntity> deviceEntities =deviceRepository.findAll();
-        System.out.println(deviceEntities);
+    void deleteDeviceTest() {
+        String token = "e_5VwVHYRJG8J3GrGdxmol:APA91bG09ABYw_kKPPBZUCCpZddOPzB2b9Edyhl562UCSqwd4uLSFmFMoDwiE0fyobpZ4TkCGw-dbIs_dyHf3rp5Gu05hCN2X-_y18GTWoR0_ZKnuIvWlh3dmIUnYqRiJEmKOf8ar7oU";
+        System.out.println(token.length());
+
+        DeviceEntity deviceToDelete = DeviceEntity.builder()
+                .deviceId(UUID.fromString("88fd8c46-e469-4a86-9e66-c1a1f33d58d5"))
+                .build();
+
+        deviceRepository.delete(deviceToDelete);
+
+        assertFalse(deviceRepository.existsById(deviceToDelete.getDeviceId()));
+    }
+    @Test
+    void ChangeDevice_token() {
+        String token = "e_5VwVHYRJG8J3GrGdxmol:APA91bG09ABYw_kKPPBZUCCpZddOPzB2b9Edyhl562UCSqwd4uLSFmFMoDwiE0fyobpZ4TkCGw-dbIs_dyHf3rp5Gu05hCN2X-_y18GTWoR0_ZKnuIvWlh3dmIUnYqRiJEmKOf8ar7oU";
+        System.out.println(token.length());
+
+        DeviceEntity deviceToDelete = DeviceEntity.builder()
+                .deviceId(UUID.fromString("88fd8c46-e469-4a86-9e66-c1a1f33d58d5"))
+                .build();
+
+        deviceRepository.delete(deviceToDelete);
+
+        assertFalse(deviceRepository.existsById(deviceToDelete.getDeviceId()));
     }
 
-    @Test
-    void patchTest(){
-        UUID userId =UUID.fromString("560dec85-8ecc-4a1b-9c2a-061924335b76");
-        DeviceDTO deviceDTO = DeviceDTO.builder()
-                .deviceToken("10")
-                .build();
-        deviceService.patchDevice(userId,deviceDTO);
-    }
+
 }
